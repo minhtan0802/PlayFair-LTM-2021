b2308daac7d1dc147c6206f99f12e87e746fa580
minhtan0802 <minhtan0802@gmail.com>
2021-11-18 20:51:23 +0700

    Update

---
+++ src/project/Client.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author minht
+ */
+public class Client {
+    public static void main(String[] args) {
+        DataInputStream din = null;
+        DataOutputStream dout = null;
+        try {
+            // TODO add your handling code here:
+            Socket client = new Socket("127.0.0.1", 8888);
+             din = new DataInputStream(client.getInputStream());
+             dout = new DataOutputStream(client.getOutputStream());
+        } catch (IOException ex) {
+           
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap key:");
+        String key=sc.nextLine();
+        System.out.println("Nhap message:");
+        String message=sc.nextLine();
+        try {
+            dout.writeUTF(key);
+            dout.writeUTF(message);
+            String ketQua = din.readUTF();
+            System.out.println("Ket qua: "+ketQua);
+            
+        } catch (IOException ex) {
+           
+        }
+        
+    }
+      
+    
+}
